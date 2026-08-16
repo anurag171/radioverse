8f0903702ababe7c92c23cc0e51f2246e3ddbb73
Anurag Mishra <59208873+anurag171@users.noreply.github.com>
2026-08-16 15:25:57 +0530

    Update support URL in config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.RADIOVERSE_CONFIG = {
   supabaseUrl: "https://cdufwrdapbyjkcbsxrwq.supabase.co",
   supabaseAnonKey: "sb_publishable_HxGxeeUdcf0xCAOrVz-vPQ_vewIDjVq",
   adminEmail: "anurag171@gmail.com",
-  supportUrl: "",
+  supportUrl: "https://anurag171.github.io/support",
 };
